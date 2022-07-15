b3ea55be2cbaa56108997b49b12ebdcc55b6e3da
Imran Molla <imranmollajoy@gmail.com>
2022-07-16 01:13:06 +0600

    fixes

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,14 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+
+		// prerender
+		prerender: {
+			enabled: true,
+			crawl: true,
+			entries: '*'
+		}
 	}
 };
 
