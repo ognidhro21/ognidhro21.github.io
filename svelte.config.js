d22e1bd612614983a138cd89f62ddd084165b192
imranmollajoy <imranmollajoy@gmail.com>
2022-08-17 13:58:25 +0600

    updated vite to avoid vercel error

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
-import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
