25dbb77f4fa9b196a54a99756073ef7249fdebf3
imranmollajoy <imranmollajoy@gmail.com>
2022-09-09 12:54:22 +0600

    moar endpoint fix

--- src/routes/schedules/+page.js
+++ src/routes/schedules/+page.js
@@ -1,9 +1,9 @@
 /**
  * @type {import("@svelte/kit").Load}
  */
-export async function load({ fetch }) {
-	const routinesList = await fetch(`/api/routines`).then((res) => res.json());
-	const examsList = await fetch(`/api/exams`).then((res) => res.json());
+export async function load({ url }) {
+	const routinesList = await fetch(`${url.origin}/api/routines`).then((res) => res.json());
+	const examsList = await fetch(`${url.origin}/api/exams`).then((res) => res.json());
 	return {
 		routinesList,
 		examsList
