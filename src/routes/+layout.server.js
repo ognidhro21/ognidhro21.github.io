f16382c145fad09ba36989e3ac06608b0cc886e9
imranmollajoy <imranmollajoy@gmail.com>
2022-09-09 13:18:17 +0600

    Fi

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,4 +1,5 @@
 // // src/routes/blog/+page.server.js
+export const prerender = true;
 export const load = async ({ url }) => {
 	const response = await fetch(`${url.origin}/api/notices`);
 	const noticeList = await response.json();
