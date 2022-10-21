7699af0bec35a30bc6b48cf282d89d8c866f28f4
Imran Molla Joy <imranmollajoy@gmail.com>
2022-10-21 11:43:07 +0600

    fixed buttons misalignment

--- src/components/fragments/homepage/Hero.svelte
+++ src/components/fragments/homepage/Hero.svelte
@@ -7,16 +7,17 @@
 		<div class="medium-space" />
 		<img src={logo} alt="" />
 		<div class="medium-space" />
-
-		<nav class="no-space">
-			<div class="small-padding border left-round">
-				<span>L2S1</span>
-			</div>
-			<a href="/exams/theory/mid/L2S1" class="button primary small-elevate no-round"
-				>Mid Schedules
-			</a>
-			<a href="/routines/L2S1" class="button border primary-border right-round">Class Routine </a>
-		</nav>
+		<div class="fixer center-align middle-align ">
+			<nav class="no-space">
+				<span class="small-padding border left-round bold">
+					<span>L2S1</span>
+				</span>
+				<a href="/exams/theory/mid/L2S1" class="button primary small-elevate no-round"
+					>Mid Schedules
+				</a>
+				<a href="/routines/L2S1" class="button border primary-border right-round">Class Routine </a>
+			</nav>
+		</div>
 	</div>
 </div>
 
