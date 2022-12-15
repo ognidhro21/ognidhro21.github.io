305dcefb07f69c216b3a01425b2194e049005649
Imran Molla Joy <imranmollajoy@gmail.com>
2022-12-15 19:04:39 +0600

    updated exam routine

--- src/components/fragments/homepage/Hero.svelte
+++ src/components/fragments/homepage/Hero.svelte
@@ -12,7 +12,8 @@
 				<span class="small-padding border left-round bold">
 					<span>L2S1</span>
 				</span> -->
-			<a href="routines/L2S1" class="button primary small-elevate ">Class Routine</a>
+			<!-- <a href="routines/L2S1" class="button primary small-elevate ">Class Routine</a> -->
+			<a href="/exams/theory/final/L2S1" class="button primary small-elevate ">L2S1 Final Exam</a>
 			<!-- <a href="/exams/theory/mid/L2S1" class="button border primary-border right-round">
 					Mid Schedules
 				</a>
