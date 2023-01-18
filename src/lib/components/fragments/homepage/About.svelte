9549b3d324e0c511e0fd9e5391e336e6e9209823
Imran Molla Joy <imranmollajoy@gmail.com>
2023-01-18 19:48:11 +0600

    updated about text

--- src/lib/components/fragments/homepage/About.svelte
+++ src/lib/components/fragments/homepage/About.svelte
@@ -6,7 +6,13 @@
 <Container fluid bgColor="var(--plain)">
 	<div class="contents">
 		<div class="text">
-			<p>WE ARE the 21st batch of Agriculture in Patuakhali Science and Technology University.</p>
+			<p>
+				We, the 21st batch of Agriculture students at Patuakhali Science and Technology University,
+				would like to express our presence and introduce ourselves to you. As current students of
+				the agriculture program at this esteemed institution, we are dedicated to gaining the
+				knowledge and skills necessary to excel in our field. We take pride in being a part of this
+				university and look forward to making valuable contributions to the field of agriculture.
+			</p>
 		</div>
 		<div class="img">
 			<img src={IMG} alt="" />
