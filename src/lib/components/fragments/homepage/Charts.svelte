b40d59b1f109e70963cc7530046348c3cc4c0763
Imran Molla Joy <imranmollajoy@gmail.com>
2023-06-13 13:07:04 +0600

    charts

---
+++ src/lib/components/fragments/homepage/Charts.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Chart from 'svelte-frappe-charts';
+
+  let data = {
+    labels: ["L1S1","L1S2","L2S1"],
+    datasets: [
+      {
+        name: "1901141",
+        values: [3.443,3.532,3.639]
+      },
+      {
+        name: "1901143",
+        values: [3.743,3.432,3.5]
+      },
+      {
+        name: "1901120",
+        values: [4,3.932,4]
+      }
+    ]
+  };
+</script>
+
+<Chart data={data} type="line" />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,11 @@
 <script>
-	import About from '$lib/components/fragments/homepage/About.svelte';
+	import Charts from '$lib/components/fragments/homepage/Charts.svelte';
 	import Forecast from '$lib/components/fragments/homepage/Forecast.svelte';
 	import Hero from '$lib/components/fragments/homepage/Hero.svelte';
 	import Links from '$lib/components/fragments/homepage/Links.svelte';
 	import MadeWith from '$lib/components/fragments/homepage/MadeWith.svelte';
 	import Ongoing from '$lib/components/fragments/homepage/Ongoing.svelte';
 	import SEO from '$lib/components/shared/SEO/index.svelte';
-	import { onMount } from 'svelte';
 	export let data;
 	const { Headline, rain, classes } = data;
 </script>
@@ -17,5 +16,4 @@
 <Forecast {Headline} {rain} />
 <!-- <Actions /> -->
 <Links />
-<!-- <About /> -->
 <MadeWith />
